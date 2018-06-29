0ff9e3d3756f995fc9c6d8b21133e8c9b74dad1c
Daniel Machado Pintos <daniel.pintos@grupoabl.com.br>
2018-06-29 08:07:23 -0300

    atualizando

--- examples/basic.py
+++ examples/basic.py
@@ -6,10 +6,18 @@ from __future__ import unicode_literals
 import sys
 from wit import Wit
 
-if len(sys.argv) != 2:
-    print('usage: python ' + sys.argv[0] + ' <wit-token>')
-    exit(1)
-access_token = sys.argv[1]
+# if len(sys.argv) != 2:
+#     print('usage: python ' + sys.argv[0] + ' <wit-token>')
+#     exit(1)
+# access_token = sys.argv[1]
+# acess server KVK4NH345RMGO4FW7U7U3PBYWPTOY2XS
+# client acess token 4MIYOOWP2X6552JH5CPPYIS6CLT63AEW
 
-client = Wit(access_token=access_token)
-client.interactive()
+if __name__ == '__main__':
+    print("start")
+    try:
+        client = Wit(access_token="KVK4NH345RMGO4FW7U7U3PBYWPTOY2XS")
+        resp = client.message("ola meu consagrado")
+        print(resp)
+    except Exception as error:
+        repr(error)
